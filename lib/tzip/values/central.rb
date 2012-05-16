d3582a0ee6904e956039a7de2f700a26ebd2a0c8
Pai-Hung Chen <phc@zestcash.com>
2012-05-16 10:55:36 -0700

    Add mappings for South Dakota.

--- lib/tzip/values/central.rb
+++ lib/tzip/values/central.rb
@@ -1,6 +1,6 @@
 module TZip
   ZONINGS[:central] = %w{35 36 39}
-  ZONINGS[:central] += %w{50 51 52 53 54 55 56}
+  ZONINGS[:central] += %w{50 51 52 53 54 55 56 570 571 572 573 574 575 576}
   ZONINGS[:central] += %w{60 61 62 63 64 65}
   ZONINGS[:central] += %w{7}
 end
--- lib/tzip/values/mountain.rb
+++ lib/tzip/values/mountain.rb
@@ -1,5 +1,6 @@
 module TZip
-  ZONINGS[:mountain] = %w{59}
+  ZONINGS[:mountain] = %w{577}
+  ZONINGS[:mountain] += %w{59}
   ZONINGS[:mountain] += %w{798 799}
   ZONINGS[:mountain] += %w{80 81 82 83 831 84 87 88}
 end
