0228b54c9cbe6c0ca4aedf59359fd3733c07eea1
Shannon McPherson <mcphersonz@gmail.com>
2012-09-14 22:14:41 -0700

    more matches

--- spec/tzip_spec.rb
+++ spec/tzip_spec.rb
@@ -19,7 +19,7 @@ describe TZip do
   end
 
   describe "Eastern" do
-    %w(00926 08701 11226 23464 28269 30043 30044 48201 48109).each{|zip|
+    %w(00926 08701 11226 23464 28269 30043 30044 48201 48109 32308).each{|zip|
       it { print zip; lookup_zip(zip).should == 'Eastern Time (US & Canada)' }
     }
   end
@@ -37,7 +37,7 @@ describe TZip do
   end
 
   describe "Pacific" do
-    %w(89110 90011 90650).each{|zip|
+    %w(89110 90011 90650 97701).each{|zip|
       it { print zip; lookup_zip(zip).should == 'Pacific Time (US & Canada)' }
     }
   end
