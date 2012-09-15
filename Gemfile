d597eb200310697ade485c4b502db259b189a80b
Shannon McPherson <mcphersonz@gmail.com>
2012-09-14 23:20:39 -0700

    added & configured Guard

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source "http://rubygems.org"
 
-gem "guard"
+gem 'debugger', '~> 1.2.0'
+gem 'rspec'
+gem "guard", '~> 1.3.2'
+gem 'guard-rspec', '~> 1.2.1'
+gem 'guard-spork', '~> 1.1.0'
+gem 'rb-fsevent', '~> 0.9.1'
+gem 'active_support'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
-require File.join(File.dirname(__FILE__), '/../lib/tzip')
+require 'rubygems'
 require 'spork'
 require 'spork/ext/ruby-debug'
+require 'active_support'
+require 'rspec'
+
+Spork.prefork do
+
+end
+
+Spork.each_run do
+  require File.join(File.dirname(__FILE__), '../lib/tzip.rb')
+  Dir.glob(File.dirname(__FILE__) + '/../lib/tzip/values/*.rb') {|file| require file}
+end
 
 def lookup_zip(zip)
   ActiveSupport::TimeZone.find_by_zipcode(zip)
